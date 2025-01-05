2a0af51c91810569c518fa5d6e760b4e12d7d2ed
AlDIY <87589371+dosipod@users.noreply.github.com>
2025-01-05 17:49:25 +0300

    Update usermods_list.cpp

--- wled00/usermods_list.cpp
+++ wled00/usermods_list.cpp
@@ -242,6 +242,12 @@
 #include "../usermods/LD2410_v2/usermod_ld2410.h"
 #endif
 
+#ifdef USERMOD_ALDIY
+#include "../usermods/usermod_aldiy/usermod_aldiy.h"
+#endif
+
+
+
 void registerUsermods()
 {
 /*
@@ -469,5 +475,9 @@ void registerUsermods()
 
   #ifdef USERMOD_POV_DISPLAY
   UsermodManager::add(new PovDisplayUsermod());
+  #endif
+
+  #ifdef USERMOD_ALDIY
+  usermods.add(new AldiyUsermod());
   #endif
 }
